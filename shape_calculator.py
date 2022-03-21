9cedeabafa28fe4f2377a3e4a6407c572b1466dd
valeriocipolla <valeriocipolla92@gmail.com>
2022-03-21 15:48:32 +0000

    refactored

--- shape_calculator.py
+++ shape_calculator.py
@@ -32,7 +32,6 @@ class Rectangle:
         result += "\n"
       return result
 
-  #todo
   def get_amount_inside(self, shape):
     object_area = self.get_area()
     shape_area = shape.get_area()
@@ -66,22 +65,3 @@ class Square(Rectangle):
   def set_width(self, width):
       self.width = width
       self.heigth = width
-
-
-#rect = Rectangle(10, 5)
-#print(rect.get_area())
-#rect.set_height(3)
-#print(rect.get_perimeter())
-#print(rect)
-#print(rect.get_picture())
-#
-#sq = Square(9)
-#print(sq.get_area())
-#sq.set_side(4)
-#print(sq.get_diagonal())
-#print(sq)
-#print(sq.get_picture())
-#
-#rect.set_height(8)
-#rect.set_width(16)
-#print(rect.get_amount_inside(sq))
